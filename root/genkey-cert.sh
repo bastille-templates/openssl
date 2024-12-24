3eb3c6f520ba7fca0e62eb3c7d3b14afd8ca15e3
Asep Ready <savalunova@gmail.com>
2024-12-24 09:25:26 +0700

    generate key ssl

--- root/genkey-cert.sh
+++ root/genkey-cert.sh
@@ -4,6 +4,7 @@ echo "Make sure you have set this script with the correct -subj content, such as
 echo "In other words, don't use this script blindly without changing the -subj content first. Press Ctrl C to cancel this operation if in need. You have 10 seconds."
 
 sleep 10
+rm -rf /usr/local/etc/ssl
 
 # Create the CA directory
 mkdir -p /usr/local/etc/ssl
@@ -34,7 +35,7 @@ echo "Server's certificate and key pair have been generated."
 echo "Generating client's key and certificate pair."
 
 # 1.- Generate a new key for the client plus a certificate request
-openssl req -newkey rsa:2048 -days 730 -nodes -keyout /usr/local/etc/ssl/client-key.pem -out /usr/local/etc/ssl/client-req.pem -subj "/C=US/ST=California/L=San Francisco/O=Adminbyaccident Ltd/CN=example.com/emailAddress=youremail@anymail.com"
+openssl req -newkey rsa:2048 -days 730 -nodes -keyout /usr/local/etc/ssl/client-key.pem -out /usr/local/etc/ssl/client-req.pem -subj "/C=US/ST=California/L=San Francisco/O=Adminbyaccident Ltd/CN=client.example.com/emailAddress=youremail@anymail.com"
 
 # 2.- Strip out the passphrase within the key
 openssl rsa -in /usr/local/etc/ssl/client-key.pem -out /usr/local/etc/ssl/client-key.pem
